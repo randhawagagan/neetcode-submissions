06c562bbed9a6535e53594a439e4af04ef8d6424
Gagan Singh <randhawa_gagan@live.com>
2026-08-10 23:11:45 -0400

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.ts
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums: number[], target: number): number[] {
+        const seen = new Map<number, number>();
+
+        for(let i = 0; i<nums.length; i++){
+            const needed = target - nums[i];
+
+            if(seen.has(needed)){
+                return [seen.get(needed)!, i];
+            }
+
+            seen.set(nums[i], i);
+        }
+
+        return [];
+    }
+}
